67625c96c35f4417eefb7b244038694a99d2bd33
EhlimanSen <ehlimansen@gmail.com>
2018-11-08 16:05:17 +0300

    düzeltme

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
 <body>
     <h1>It works!</h1>
-    <h2>Simple HTTP + PHP server</h2>
-    <a href="https://github.com/HasHolding/php-simple">github.com/HasHolding/php-simple</a>
+    <h2>Simple HTTP (Apache)+ PHP5 server</h2>
+    <a href="https://github.com/NetYazilim/php-simple">github.com/NetYazilim//php-simple</a>
     <br/>
     <?php echo "<h2>PHP works!</h2><br/>"; ?>
     <a href="/info.php">PHP info</a>
